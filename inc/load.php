2ce2c962f943710d0189c9de7fe040a3d1e88e7c
Gustavo Bordoni <webord.net@gmail.com>
2014-04-21 05:23:03 -0300

    Just a Development View

--- inc/load.php
+++ inc/load.php
@@ -7,6 +7,9 @@ require_once plugin_dir_path( __FP_FILE__ ) . 'classes' . DIRECTORY_SEPARATOR . 
 // Create the Plugin static instance
 $FakerPress = new Plugin;
 
+// Include the Faker Main Class
+require_once Plugin::path( 'inc/vendor/Faker/src/autoload.php' );
+
 // Require our Administration Class
 require_once Plugin::path( 'classes/admin.php' );
 
---
+++ view/dev.php
@@ -0,0 +1,4 @@
+<div class='wrap'>
+    <h2><?php esc_attr_e( 'Development View', 'fakerpress' ); ?></h2>
+
+</div>
