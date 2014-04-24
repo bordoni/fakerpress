60c8132713e4b6dd56b262c03c2f39fe28c6347c
Gustavo Bordoni <webord.net@gmail.com>
2014-04-24 13:15:07 -0300

    Base Module, with some changes to how the arguments works

--- modules/base.php
+++ modules/base.php
@@ -10,19 +10,35 @@ abstract class Base {
 
 	public $faker = null;
 
+	public static $instance = null;
+
+	public $args = array();
+
+	public $faked_args = array();
+
 	/**
 	 * Method that will add the Faker Provider and save the $intance to the $faker var
 	 * @return object|WP_Error Should return an error or a Faker provider
 	 */
-	abstract public function __construct( $arguments );
+	abstract public function __construct();
 
 	final public function __get( $name ){
 		return $this->faker->$name;
+	}
+
+	final public function __call( $name, $arguments ){
+		return call_user_func_array( array( $this->faker, $name ), $arguments );
+	}
+
+	final public static function __callStatic( $name, $arguments ){
+		$_class = get_called_class();
+		null === self::$instance and self::$instance = new $_class();
+		return call_user_func_array( array( self::$instance->faker, $name ), $arguments );
 	}
 
 	/**
 	 * Use this method to save the fake data to the database
 	 * @return int|bool|WP_Error Should return an error, or the $wpdb->insert_id or bool for the state
 	 */
-	abstract public function save( $faker );
+	abstract public function save();
 }
