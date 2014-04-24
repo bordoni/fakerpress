1492f0a38b4d91180b424cb1022f234ef561c167
Gustavo Bordoni <webord.net@gmail.com>
2014-04-24 13:15:33 -0300

    WordPress Post module, post_title and a draft of post_content

--- modules/post.php
+++ modules/post.php
@@ -5,14 +5,33 @@ include_once \Fakerpress\Plugin::path( 'providers/post.php' );
 
 class Post extends Base {
 
-	public function __construct( $arguments ) {
+	public $faked_args = array(
+		'post_title' => null,
+		'post_content' => null,
+	);
+
+	public $args = array(
+		'post_type' => 'post',
+		'post_status' => 'publish',
+	);
+
+	public function __construct( $args = array() ) {
 		$this->faker = \Faker\Factory::create( 'en_US' );
-		$provider    = new \Faker\Provider\Post( $this->faker );
+		$provider    = new \Faker\Provider\Post( $this );
 
 		$this->faker->addProvider( $provider );
+
+		$this->args = apply_filters( 'fakerpress.module.post.args', wp_parse_args( array_merge( $this->args, $this->faked_args ) , $args ) );
 	}
 
-	public function save( $faker ) {
+	public function save() {
 
+		foreach ( $this->faked_args as $key => $value ) {
+			if ( is_null( $value ) ) {
+				$this->faker->$key();
+			}
+		}
+
+		return wp_insert_post( $this->args );
 	}
 }
