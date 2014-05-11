3e8d9e48598549a13ee8cabd5aee4dbe47b2eac0
Gustavo Bordoni <bordoni.dev@gmail.com>
2014-05-11 15:37:05 -0400

    Make the Post module compatible to the new Base style

--- modules/post.php
+++ modules/post.php
@@ -3,33 +3,26 @@ namespace FakerPress\Module;
 
 class Post extends Base {
 
-	public $faked_args = array(
-		'post_title' => null,
-		'post_content' => null,
+	public $faked = array(
+		'post_title',
+		'post_content',
+		'post_type',
+		'user_id',
+		'post_date',
 	);
 
-	public $args = array(
-		'post_type' => 'post',
-		'post_status' => 'publish',
+	public $dependencies = array(
+		'\Faker\Provider\Lorem',
+		'\Faker\Provider\DateTime',
+		'\Faker\Provider\Html',
+		'\Faker\Provider\WP_Post',
 	);
 
-	public function __construct( $args = array() ) {
-		$this->faker = \Faker\Factory::create( 'en_US' );
+	public function init() {
 
-		$provider = new \Faker\Provider\Post( $this );
-		$this->faker->addProvider( $provider );
-
-		$this->args = apply_filters( 'fakerpress.module.post.args', wp_parse_args( array_merge( $this->args, $this->faked_args ) , $args ) );
 	}
 
 	public function save() {
-
-		foreach ( $this->faked_args as $key => $value ) {
-			if ( is_null( $value ) ) {
-				$this->faker->$key();
-			}
-		}
-
 		return wp_insert_post( $this->args );
 	}
 }
