1f8d9aeecbbb1cae2a551d1e38d97a00d4deb7b3
Gustavo Bordoni <bordoni.dev@gmail.com>
2014-05-11 15:39:43 -0400

    Posts view withe the new module style

--- view/posts.php
+++ view/posts.php
@@ -1,3 +1,12 @@
 <div class='wrap'>
-    <h2><?php esc_attr_e( 'Create Fake Posts', 'fakerpress' ); ?></h2>
+	<h2><?php esc_attr_e( 'Create Fake Posts', 'fakerpress' ); ?></h2>
+
+	<?php
+		$faker = new \FakerPress\Module\Post();
+		$faker->generate(
+			array(
+				'post_type' => array( 'page', 'post' ),
+			)
+		);
+	?>
 </div>
