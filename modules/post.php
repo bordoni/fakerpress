5530952608ebe8d3667e1dd8d6cf375b6f96614a
Gustavo Bordoni <bordoni.dev@gmail.com>
2014-05-13 16:24:20 -0400

    Post Module following the new Structure of Base Modules

--- modules/post.php
+++ modules/post.php
@@ -3,26 +3,13 @@ namespace FakerPress\Module;
 
 class Post extends Base {
 
-	public $faked = array(
-		'post_title',
-		'post_content',
-		'post_type',
-		'user_id',
-		'post_date',
-		'post_status',
-		'ping_status',
-	);
-
 	public $dependencies = array(
 		'\Faker\Provider\Lorem',
 		'\Faker\Provider\DateTime',
 		'\Faker\Provider\Html',
-		'\Faker\Provider\WP_Post',
 	);
 
-	public function init() {
-
-	}
+	public $provider = '\Faker\Provider\WP_Post';
 
 	public function save() {
 		return wp_insert_post( $this->args );
