36bcb28bccf5fd8f7684bc928204a80b57643f38
Luan Cuba <luancuba9@gmail.com>
2014-05-20 23:54:20 -0300

    Comment Provider/Module and View refactored to the new default

--- modules/comment.php
+++ modules/comment.php
@@ -3,36 +3,11 @@ namespace FakerPress\Module;
 
 class Comment extends Base {
 
-	public $faked_args = array(
-		'comment_content' => null,
-		'user_id'		  => null,
-		'comment_author'  => null,
-		'comment_author_email' => null,
-		'comment_author_url' => null,
-		'comment_approved' => null,
-		'comment_post_ID'     => null,
-	);
-
-	public $args = array();
-
-	public function __construct( $args = array() ) {
-		$this->faker = \Faker\Factory::create();
-
-		$provider = new \Faker\Provider\Comment( $this );
-		$this->faker->addProvider( $provider );
-
-		$this->args = apply_filters( 'fakerpress.module.comment.args', wp_parse_args( array_merge( $this->args, $this->faked_args ) , $args ) );
-	}
+	public $provider = '\Faker\Provider\WP_Comment';
 
 	public function save() {
-
-		foreach ( $this->faked_args as $key => $value ) {
-			if ( is_null( $value ) ) {
-				$this->faker->$key();
-			}
-		}
-
 		// Here you should use the `$this->args['param_name']`
+		//var_dump( $this->args );
 		return wp_insert_comment( $this->args );
 	}
 }
