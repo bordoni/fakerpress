c72b2bc40e1bd938ee6770c7688174c817fbcb26
Gustavo Bordoni <webord.net@gmail.com>
2014-06-20 19:25:15 -0300

    Include the JS and CSS related to Select2

--- classes/admin.php
+++ classes/admin.php
@@ -297,11 +297,25 @@ Class Admin {
 	 * @return null Actions do not return
 	 */
 	public function _action_enqueue_ui() {
+		// Register the plugin CSS files
 		wp_register_style( 'fakerpress.icon', Plugin::url( 'ui/css/font.css' ), array(), Plugin::version, 'screen' );
 		wp_register_style( 'fakerpress.messages', Plugin::url( 'ui/css/messages.css' ), array(), Plugin::version, 'screen' );
 
+		// Register the plugin JS files
+		wp_register_script( 'fakerpress.fields', Plugin::url( 'ui/js/fields.js' ), array( 'jquery', 'fakerpress.select2' ), Plugin::version, true );
+
+		// Register Vendor Select2
+		wp_register_style( 'fakerpress.select2', Plugin::url( 'ui/vendor/select2/select2.css' ), array(), '3.5.0', 'screen' );
+		wp_register_style( 'fakerpress.select2-wordpress', Plugin::url( 'ui/vendor/select2/select2-wordpress.css' ), array( 'fakerpress.select2' ), '3.5.0', 'screen' );
+		wp_register_script( 'fakerpress.select2', Plugin::url( 'ui/vendor/select2/select2.min.js' ), array( 'jquery' ), '3.5.0', true );
+
+		// Enqueue plugin CSS
 		wp_enqueue_style( 'fakerpress.icon' );
 		wp_enqueue_style( 'fakerpress.messages' );
+
+		// Enqueue Vendor Select2
+		wp_enqueue_style( 'fakerpress.select2-wordpress' );
+		wp_enqueue_script( 'fakerpress.fields' );
 	}
 
 	/**
