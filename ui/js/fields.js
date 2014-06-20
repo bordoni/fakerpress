5d458bf28b9bb147b2472c5720ad482cc4aba269
Gustavo Bordoni <webord.net@gmail.com>
2014-06-20 19:23:30 -0300

    Include Select2 and Fields.js (working taxonomy field with Select2)

---
+++ ui/js/fields.js
@@ -0,0 +1,17 @@
+// Taxonomies Fields
+( function( $ ){
+	'use strict';
+	$(document).ready(function(){
+		$( '.field-select2-taxonomies' ).each(function(){
+			var $select = $(this);
+
+			$select.select2({
+				multiple: true,
+				width: 400,
+				data: function(){
+					return { 'results': $select.data( 'possibilities' ) };
+				}
+			});
+		});
+	});
+}( jQuery ) );
