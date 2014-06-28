7de4e0936883747f29d2d1efd4c3b57d9e0ef42e
Gustavo Bordoni <webord.net@gmail.com>
2014-06-28 17:08:37 -0300

    Saving references to be able to delete the data later on

--- modules/term.php
+++ modules/term.php
@@ -14,6 +14,24 @@ class Term extends Base {
 			'description' => $this->params['description'],
 			'parent' => $this->params['parent_term'],
 		);
-		return wp_insert_term( $this->params['name'], $this->params['taxonomy'], $args );
+
+		$term_object = wp_insert_term( $this->params['name'], $this->params['taxonomy'], $args );
+
+		$flagged = get_option( 'fakerpress.module_flag.' . $this->slug , array() );
+
+		// Ensure that this option is an Array by reseting the variable.
+		if ( ! is_array( $flagged ) ){
+			$flagged = array();
+		}
+
+		if ( ! isset( $flagged[ $this->params['taxonomy'] ] ) || ! is_array( $flagged[ $this->params['taxonomy'] ] ) ){
+			$flagged[ $this->params['taxonomy'] ] = array();
+		}
+		$flagged[ $this->params['taxonomy'] ] = array_merge( $flagged[ $this->params['taxonomy'] ], (array) $term_object['term_id'] );
+
+		// When in posts relating is harder so we store in the Options Table
+		update_option( 'fakerpress.module_flag.' . $this->slug, $flagged );
+
+		return $term_object;
 	}
 }
