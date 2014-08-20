53fc2caae1b3a37afe1fafcddfb52d3ec4266399
Gustavo Bordoni <bordoni.dev@gmail.com>
2014-08-20 17:26:33 -0300

    Parsing the post_parents on the posts creation

--- inc/init/posts.php
+++ inc/init/posts.php
@@ -26,6 +26,7 @@ add_action(
 
 			$taxonomies = array_intersect( get_taxonomies( array( 'public' => true ) ), array_map( 'trim', explode( ',', Filter::super( INPUT_POST, 'fakerpress_taxonomies', FILTER_SANITIZE_STRING ) ) ) );
 
+			$post_parents = array_map( 'trim', explode( ',', Filter::super( INPUT_POST, 'fakerpress_post_parents', FILTER_SANITIZE_STRING ) ) );
 
 			if ( $quantity === 0 ){
 				return Admin::add_message( sprintf( __( 'Zero is not a good number of %s to fake...', 'fakerpress' ), 'posts' ), 'error' );
@@ -41,6 +42,7 @@ add_action(
 						'tax_input' => array( $taxonomies ),
 						'post_status' => array( array( 'publish' ) ),
 						'post_date' => array( $min_date, $max_date ),
+						'post_parent' => array( $post_parents ),
 						'post_type' => array( 'post' ),
 						'post_author'   => array( $post_author ),
 						'post_type' => array( $post_types ),
