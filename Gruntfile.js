7691fab85bef3db3d6ccff72111f9c29accde72a
Gustavo Bordoni <bordoni.dev@gmail.com>
2015-05-11 10:24:19 -0300

    Using Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,160 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+	'use strict';
+
+	grunt.initConfig({
+
+		// Setting folder templates.
+		dirs: {
+			css: 'ui/css',
+			fonts: 'ui/font',
+			images: 'ui/img',
+			js: 'ui/js'
+		},
+
+		// JavaScript linting with JSHint.
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			all: [
+				'Gruntfile.js',
+				'<%= dirs.js %>/*.js',
+			]
+		},
+
+		// Minify .js files.
+		uglify: {
+			core: {
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.js %>/',
+					src: [
+						'*.js',
+						'!*.min.js',
+						'!Gruntfile.js',
+					],
+					dest: '<%= dirs.js %>/',
+					ext: '.min.js'
+				}]
+			},
+		},
+
+		// Compile all .scss files.
+		sass: {
+			compile: {
+				options: {
+					sourcemap: 'none',
+					loadPath: require( 'node-bourbon' ).includePaths
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= dirs.css %>/',
+					src: ['*.scss'],
+					dest: '<%= dirs.css %>/',
+					ext: '.css'
+				}]
+			}
+		},
+
+		// Minify all .css files.
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd: '<%= dirs.css %>/',
+				src: ['*.css'],
+				dest: '<%= dirs.css %>/',
+				ext: '.css'
+			}
+		},
+
+		// Watch changes for assets.
+		watch: {
+			css: {
+				files: ['<%= dirs.css %>/*.scss'],
+				tasks: ['sass', 'cssmin']
+			},
+			js: {
+				files: [
+					'<%= dirs.js %>/*js',
+					'!<%= dirs.js %>/*.min.js',
+				],
+				tasks: ['uglify']
+			}
+		},
+
+		// Generate POT files.
+		makepot: {
+			core: {
+				type: 'wp-plugin',
+				domainPath: 'l10n',
+				potHeaders: {
+					'report-msgid-bugs-to': 'https://github.com/bordoni/fakerpress/issues',
+					'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+				}
+			},
+		},
+
+		// Check textdomain errors.
+		checktextdomain: {
+			options:{
+				text_domain: 'woocommerce',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!bin/**', // Exclude apigen/
+					'!apigen/**', // Exclude apigen/
+					'!node_modules/**', // Exclude node_modules/
+					'!tests/**', // Exclude tests/
+					'!tmp/**' // Exclude tmp/
+				],
+				expand: true
+			}
+		},
+
+	});
+
+	// Load NPM tasks to be used here
+	grunt.loadNpmTasks( 'grunt-shell' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-sass' );
+	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [
+		'css',
+		'uglify'
+	]);
+
+	grunt.registerTask( 'css', [
+		'sass',
+		'cssmin'
+	]);
+
+	grunt.registerTask( 'dev', [
+		'default',
+		'makepot'
+	]);
+};
