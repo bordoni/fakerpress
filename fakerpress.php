1c6a272c8c76d4ea705dd4569151000a486434c8
bordoni <bordoni.dev@gmail.com>
2017-05-28 03:24:32 -0300

    Version 0.4.8 on info

--- fakerpress.php
+++ fakerpress.php
@@ -3,7 +3,7 @@
  * Plugin Name:       FakerPress
  * Plugin URI:        https://fakerpress.com
  * Description:       FakerPress is a clean way to generate fake data to your WordPress instalation, great for developers who need testing
- * Version:           0.4.7
+ * Version:           0.4.8
  * Author:            Gustavo Bordoni
  * Author URI:        http://bordoni.me
  * Text Domain:       fakerpress
