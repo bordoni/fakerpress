9e214ddd7cac82e80bf79c9a0109a592fb3b31b5
bordoni <bordoni.dev@gmail.com>
2017-07-22 14:56:09 -0300

    Add docblocks to the PlaceHoldIt provider

--- providers/image/placeholdit.php
+++ providers/image/placeholdit.php
@@ -1,14 +1,33 @@
 <?php
 namespace Faker\Provider;
 
+/**
+ * @since  0.1.5
+ */
 class PlaceHoldIt extends Base {
 	/**
-	 * @param \Faker\Generator $generator
+	 * Constructor for the Provider
+	 *
+	 * @since  0.1.5
+	 *
+	 * @param \Faker\Generator $generator An instance of the Faker Generator class
 	 */
 	public function __construct( \Faker\Generator $generator ) {
 		$this->generator = $generator;
 	}
 
+	/**
+	 * Generates a URL for Placehold.it
+	 *
+	 * @since  0.1.5
+	 *
+	 * @param  array|int  $width  A range for the images that will be generated, if a int is passed
+	 *                            we use that value always.
+	 * @param  float      $ratio  It makes the logic a few times easier to pass the Ratio instead of
+	 *                            the height of the image.
+	 *
+	 * @return string
+	 */
 	public function placeholdit( $width = array( 200, 640 ), $ratio = 1.25 ) {
 		if ( is_array( $width ) ){
 			$width = call_user_func_array( array( $this->generator, 'numberBetween' ), $width );
@@ -20,5 +39,4 @@ class PlaceHoldIt extends Base {
 
 		return $url;
 	}
-
 }
