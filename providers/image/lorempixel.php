6781eef5c353ed5911e9fe054a83e4e13d6e85d2
bordoni <bordoni.dev@gmail.com>
2017-07-22 14:54:51 -0300

    Add docblocks on LoremPixel

--- providers/image/lorempixel.php
+++ providers/image/lorempixel.php
@@ -1,14 +1,35 @@
 <?php
 namespace Faker\Provider;
 
+/**
+ * Provides images from Lorem Pixel
+ *
+ * @since  0.3.2
+ */
 class LoremPixel extends Base {
 	/**
-	 * @param \Faker\Generator $generator
+	 * Constructor for the LoremPixel provider
+	 *
+	 * @since  0.3.2
+	 *
+	 * @param \Faker\Generator $generator An instance of the Faker Generator class
 	 */
 	public function __construct( \Faker\Generator $generator ) {
 		$this->generator = $generator;
 	}
 
+	/**
+	 * Generate an URL for an image from LoremPixel
+	 *
+	 * @since  0.3.2
+	 *
+	 * @param  array|int  $width  A range for the images that will be generated, if a int is passed
+	 *                            we use that value always.
+	 * @param  float      $ratio  It makes the logic a few times easier to pass the Ratio instead of
+	 *                            the height of the image.
+	 *
+	 * @return string        Return the URL for the
+	 */
 	public function lorempixel( $width = array( 200, 640 ), $ratio = 1.25 ) {
 		if ( is_array( $width ) ){
 			$width = call_user_func_array( array( $this->generator, 'numberBetween' ), $width );
@@ -16,6 +37,7 @@ class LoremPixel extends Base {
 
 		$width = absint( $width );
 		$height = floor( $width / floatval( $ratio ) );
+
 		$categories = array(
 			'abstract',
 			'animals',
@@ -31,6 +53,14 @@ class LoremPixel extends Base {
 			'technics',
 			'transport',
 		);
+
+		/**
+		 * Allow developers to filter the Categories, only one will be selected.
+		 *
+		 * @param  array  $categories  The set of categories that can be used from LoremPixel
+		 * @param  self   $provider    An instance of the Provider
+		 */
+		$categories = (array) apply_filters( 'fakerpress.provider.image.lorempixel.categories', $categories, $this );
 
 		$url = "http://lorempixel.com/{$width}/{$height}/" . $this->generator->randomElement( $categories );
 		return $url;
